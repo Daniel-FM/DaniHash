731a92943a569e7727e88804e0a43814bb586a16
Tiodani <39848123+Tiodani@users.noreply.github.com>
2018-07-02 01:26:33 -0300

    Adicionando headers e número de versão

--- DaniHash_headers/EstrAuxiliares.h
+++ DaniHash_headers/EstrAuxiliares.h
@@ -1,3 +1,11 @@
+/*
+ * File:   EstrAuxiliares.h
+ * Author: Daniel Franklin
+ *
+ * Created on 2018
+ * Part of the DaniHash project
+ */
+
 #ifndef ESTRAUXILIARES_H_INCLUDED
 #define ESTRAUXILIARES_H_INCLUDED
 

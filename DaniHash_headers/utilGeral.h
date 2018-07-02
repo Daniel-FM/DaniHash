3752ffc1984a234e8d600b7584bbb826f883cfdc
Tiodani <39848123+Tiodani@users.noreply.github.com>
2018-07-02 01:31:37 -0300

    Adicionando headers e número de versão

--- DaniHash_headers/utilGeral.h
+++ DaniHash_headers/utilGeral.h
@@ -1,3 +1,11 @@
+/*
+ * File:   utilGeral.h
+ * Author: Daniel Franklin
+ *
+ * Created on 2018
+ * Part of the DaniHash project
+ */
+
 #ifndef FUNCOES_AUXILIARES_GERAL_H_INCLUDED
 #define FUNCOES_AUXILIARES_GERAL_H_INCLUDED
 #include <iostream>
